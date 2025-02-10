772571fbc827c7434e76cd04fa52ce9c2deea1a0
Haim <adsyandex@zohomail.com>
2025-02-10 17:21:07 +1000

    Update storage.go
    Add  package storage

---
+++ todo-app/internal/storage/storage.go
@@ -0,0 +1,16 @@
+package storage
+
+import "todo-app/internal/task"
+
+var ErrTaskNotFound = errors.New("задача не найдена")
+
+// Storage определяет интерфейс для работы с хранилищем задач.
+type Storage interface {
+    SaveTask(task.Task) error
+    GetTasks() ([]task.Task, error)
+    GetTaskByID(id int) (task.Task, error)
+    UpdateTask(task.Task) error
+    DeleteTask(id int) error
+    GetNextID() int
+}
+
